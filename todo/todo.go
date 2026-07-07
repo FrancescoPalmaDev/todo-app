a0f8f38bbf5514417b3d1c6836090aef4f33d9ae
Francesco Palma <117196189+FrancescoPalmaDev@users.noreply.github.com>
2026-07-07 20:44:31 +0200

    loading jsons

--- todo/todo.go
+++ todo/todo.go
@@ -17,8 +17,16 @@ type ToDoList struct {
 
 func (t *ToDoList) Load() error {
 	data, err := os.ReadFile(t.FilePath)
-	if err == nil {
+	if err != nil {
 		return nil
 	}
 	return json.Unmarshal(data, &t.Tasks)
+}
+
+func (t *ToDoList) Save() error {
+	data, err := json.Marshal(t.Tasks)
+	if err == nil {
+		err = os.WriteFile(t.FilePath, data, 0644)
+	}
+	return err
 }
